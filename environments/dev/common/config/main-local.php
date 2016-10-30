628054463db6867ba9225ceb7d0656f09735a0a1
Sathit Seethaphon <dixonsatit@gmail.com>
2016-10-31 01:08:07 +0700

    เพิ่ม smtp ในการส่งเมล์

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -14,7 +14,15 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'username@gmail.com',
+                'password' => 'password',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
     ],
 ];
--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -11,6 +11,15 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'username@gmail.com',
+                'password' => 'password',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
     ],
 ];
