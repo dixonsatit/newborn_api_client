7abbd669ff02012f7fd6ce5ac6e6d2e127023f09
Sathit Seethaphon <dixonsatit@gmail.com>
2016-10-31 14:05:58 +0700

    เพิ่ม minify view และ css

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -92,6 +92,20 @@ return [
             ],
         ],
         'view' => [
+            'class' => '\rmrevin\yii\minify\View',
+            'enableMinify' => !YII_DEBUG,
+            'web_path' => '@web', // path alias to web base
+            'base_path' => '@webroot', // path alias to web base
+            'minify_path' => '@webroot/minify', // path alias to save minify result
+            'js_position' => [\yii\web\View::POS_END], // positions of js files to be minified
+            'force_charset' => 'UTF-8', // charset forcibly assign, otherwise will use all of the files found charset
+            'expand_imports' => true, // whether to change @import on content
+            'compress_output' => true, // compress result html page
+            'compress_options' => ['extra' => true], // options for compress
+            'concatCss' => true, // concatenate css
+            'minifyCss' => true, // minificate css
+            'concatJs' => true, // concatenate js
+            'minifyJs' => true, // minificate js
             'theme' => [
                 'pathMap' => [
                     '@dektrium/user/views' => '@common/views/user',
