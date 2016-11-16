fe0ae770c957a470b6a4d7846b156f563b378d29
Sathit Seethaphon <dixonsatit@gmail.com>
2016-11-16 10:39:59 +0700

    ปรับปรุงชื่อ app

--- api/views/layouts/main.php
+++ api/views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'Newborn API',
+        'brandLabel' => 'NEWBORN API',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-default navbar-fixed-top',
